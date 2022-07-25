22f8f749ba55c2768a48ed0ec47425cdc27a1e01
DeepDive <71082215+RspSang@users.noreply.github.com>
2022-07-25 17:39:43 +0900

    Update site info

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'a8db8a7f3f5142bcbf37c2f5ecf7e273',
+  rootNotionPageId: '36243b321d4e4ffe858764169d112f81',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -21,7 +21,7 @@ export default siteConfig({
   github: 'RspSang',
   // linkedin: 'fisch2',
   // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  youtube: 'channel/UCUwHQZ8maprZMMlMvPQMKzw', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -30,7 +30,7 @@ export default siteConfig({
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
-  isPreviewImageSupportEnabled: true,
+  isPreviewImageSupportEnabled: false,
 
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
@@ -41,24 +41,29 @@ export default siteConfig({
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: {
+    '/dev': '2af9201764334fe3be15cc9b6d03de00',
+    '/life': '24269821b727424fa671c4a54809869d',
+    '/about': '8ea1f994a6a54a33b83be22a4027888b'
+  },
+  // pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'Dev',
+      pageId: '2af9201764334fe3be15cc9b6d03de00'
+    },
+    {
+      title: 'Life',
+      pageId: '24269821b727424fa671c4a54809869d'
+    },
+    {
+      title: 'About',
+      pageId: '8ea1f994a6a54a33b83be22a4027888b'
+    }
+  ]
 })
